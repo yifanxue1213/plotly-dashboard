7efa3d0e2d03d920c42d31d0d4e863bccc127ecf
peiyuanqi <qipeiyuan@outlook.com>
2018-10-31 15:09:59 -0700

    change tmp bar chart

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State, Event
 import plotly.plotly as py
-from plotly.graph_objs import *
-from plotly.graph_objs import layout as plotlyLayout
+import plotly.graph_objs as go
 from scipy.stats import rayleigh
 from flask import Flask
 import numpy as np
@@ -45,31 +44,9 @@ app.layout = html.Div([
                 html.H3("Power (W)")
             ], className='Title'),
             html.Div([
-                # slider
-                html.Div([
-                    dcc.Slider(
-                        id='bin-slider',
-                        min=1,
-                        max=60,
-                        step=1,
-                        value=20,
-                        updatemode='drag'
-                    ),
-                ], className='histogram-slider'),
-                html.P('# of Bins: Auto', id='bin-size', className='bin-size'),
-                html.Div([
-                    dcc.Checklist(
-                        id='bin-auto',
-                        options=[
-                            {'label': 'Auto', 'value': 'Auto'}
-                        ],
-                        values=['Auto']
-                    ),
-                ], className='bin-auto'),
-                    dcc.Graph(id='dynamic-power'),
-                ], className='dynamic-power'),
-                # histogram of power
-                dcc.Interval(id='dynamic-power-update', interval=1000, n_intervals=0),
+                dcc.Graph(id='dynamic-power'),
+            ], className='dynamic-power'),
+            dcc.Interval(id='dynamic-power-update', interval=1000, n_intervals=0),
         ], className='six columns dynamic-line-row')
     ], className='row wind-histo-polar'),
     html.Div([
@@ -101,22 +78,10 @@ def get_temp(interval):
                             rowid > "{}" AND rowid <= "{}";'
                             .format(total_time-200, total_time), con)
 
-    trace = Scatter(
-        y=df['Speed'],
-        mode='markers'
-    )
-
-    layout = Layout(
+    layout = go.Layout(
         height=450,
         xaxis=dict(
-            range=[0, 200],
-            showgrid=False,
-            showline=False,
-            zeroline=False,
-            fixedrange=True,
-            tickvals=[0, 50, 100, 150, 200],
-            ticktext=['200', '150', '100', '50', '0'],
-            title='Time Elapsed (sec)'
+            title='Temperature Sensors'
         ),
         yaxis=dict(
             range=[min(0, min(df['Speed'])),
@@ -126,14 +91,37 @@ def get_temp(interval):
             zeroline=False,
             nticks=max(6, round(df['Speed'].iloc[-1]/10))
         ),
-        margin=plotlyLayout.Margin(
+        margin=go.layout.Margin(
             t=45,
             l=50,
             r=50
         )
     )
 
-    return Figure(data=[trace], layout=layout)
+    numOfTempSensors = 3
+    uscRed = 'rgba(142, 26, 17, 1)'
+    normalBlue = 'rgba(54, 119, 175, 1)'
+    colors = [normalBlue] * numOfTempSensors
+    tempLowerBound = 8
+    tempUpperBound = 20
+
+    for i in range(0, numOfTempSensors):
+        if (df['Speed'][i] < tempLowerBound) or (df['Speed'][i] > tempUpperBound):
+            colors[i] = uscRed
+        else:
+            colors[i] = normalBlue
+    trace = go.Bar(
+        x=['c3','b2','a1'],
+        y=[df.iloc[-3]['Speed'],df.iloc[-2]['Speed'],df.iloc[-1]['Speed']],
+        hoverinfo='y',
+        text=[df.iloc[-3]['Speed'],df.iloc[-2]['Speed'],df.iloc[-1]['Speed']],
+        textposition='auto',
+        marker=dict(
+            color=colors
+        ),
+    )
+
+    return go.Figure(data=[trace], layout=layout)
 
 
 # Callback for voltage update
@@ -151,15 +139,15 @@ def get_voltage(interval):
                             rowid > "{}" AND rowid <= "{}";'
                             .format(total_time-200, total_time), con)
 
-    trace = Scatter(
+    trace = go.Scatter(
         y=df['Speed'],
-        line=scatter.Line(
+        line= go.scatter.Line(
             color='#990000'
         ),
         mode='lines'
     )
 
-    layout = Layout(
+    layout = go.Layout(
         height=450,
         xaxis=dict(
             range=[0, 200],
@@ -179,22 +167,20 @@ def get_voltage(interval):
             zeroline=False,
             nticks=max(6, round(df['Speed'].iloc[-1]/10))
         ),
-        margin=plotlyLayout.Margin(
+        margin=go.layout.Margin(
             t=45,
             l=50,
             r=50
         )
     )
 
-    return Figure(data=[trace], layout=layout)
+    return go.Figure(data=[trace], layout=layout)
 
 
 # Callback for power update
 @app.callback(Output('dynamic-power', 'figure'),
-              [Input('dynamic-power-update', 'n_intervals')],
-              [State('bin-slider', 'value'),
-               State('bin-auto', 'values')])
-def get_power(interval, sliderValue, auto_state):
+              [Input('dynamic-voltage-update', 'n_intervals')])
+def get_power(interval):
     # get power data
     now = dt.datetime.now()
     sec = now.second
@@ -208,151 +194,42 @@ def get_power(interval, sliderValue, auto_state):
                                 rowid > "{}" AND rowid <= "{}";'
                            .format(total_time - 200, total_time), con)
 
-    # plot histogram
-    wind_val = df['Speed']
-    if 'Auto' in auto_state:
-        bin_val = np.histogram(wind_val, bins=range(int(round(min(wind_val))),
-                                                    int(round(max(wind_val)))))
-    else:
-        bin_val = np.histogram(wind_val, bins=sliderValue)
-
-    avg_val = float(sum(wind_val)) / len(wind_val)
-    median_val = np.median(wind_val)
-
-    pdf_fitted = rayleigh.pdf(bin_val[1], loc=(avg_val) * 0.55,
-                              scale=(bin_val[1][-1] - bin_val[1][0]) / 3)
-
-    y_val = pdf_fitted * max(bin_val[0]) * 20,
-    y_val_max = max(y_val[0])
-    bin_val_max = max(bin_val[0])
-
-    trace = Bar(
-        x=bin_val[1],
-        y=bin_val[0],
-        marker=bar.Marker(
-            color='#7F7F7F'
+    trace = go.Scatter(
+        y=df['Speed'],
+        line= go.scatter.Line(
+            color='#990000'
         ),
-        showlegend=False,
-        hoverinfo='x+y'
+        mode='lines'
     )
-    trace1 = Scatter(
-        x=[bin_val[int(len(bin_val) / 2)]],
-        y=[0],
-        mode='lines',
-        line=scatter.Line(
-            dash='dash',
-            color='#2E5266'
-        ),
-        marker=scatter.Marker(
-            opacity=0,
-        ),
-        visible=True,
-        name='Average'
-    )
-    trace2 = Scatter(
-        x=[bin_val[int(len(bin_val) / 2)]],
-        y=[0],
-        line=scatter.Line(
-            dash='dot',
-            color='#BD9391'
-        ),
-        mode='lines',
-        marker=scatter.Marker(
-            opacity=0,
-        ),
-        visible=True,
-        name='Median'
-    )
-    trace3 = Scatter(
-        mode='lines',
-        line=scatter.Line(
-            color='#42C4F7'
-        ),
-        y=y_val[0],
-        x=bin_val[1][:len(bin_val[1])],
-        name='Rayleigh Fit'
-    )
-    layout = Layout(
+
+    layout = go.Layout(
+        height=450,
         xaxis=dict(
-            title='Wind Speed (mph)',
-            showgrid=False,
-            showline=False,
-            fixedrange=True
-        ),
-        yaxis=dict(
+            range=[0, 200],
             showgrid=False,
             showline=False,
             zeroline=False,
-            title='Number of Samples',
-            fixedrange=True
+            fixedrange=True,
+            tickvals=[0, 50, 100, 150, 200],
+            ticktext=['200', '150', '100', '50', '0'],
+            title='Time Elapsed (sec)'
         ),
-        margin=plotlyLayout.Margin(
-            t=50,
-            b=20,
+        yaxis=dict(
+            range=[min(0, min(df['Speed'])),
+                   max(45, max(df['Speed'])+max(df['SpeedError']))],
+            showline=False,
+            fixedrange=True,
+            zeroline=False,
+            nticks=max(6, round(df['Speed'].iloc[-1]/10))
+        ),
+        margin=go.layout.Margin(
+            t=45,
+            l=50,
             r=50
-        ),
-        autosize=True,
-        bargap=0.01,
-        bargroupgap=0,
-        hovermode='closest',
-        legend=plotlyLayout.Legend(
-            x=0.175,
-            y=-0.2,
-            orientation='h'
-        ),
-        shapes=[
-            dict(
-                xref='x',
-                yref='y',
-                y1=int(max(bin_val_max, y_val_max)) + 0.5,
-                y0=0,
-                x0=avg_val,
-                x1=avg_val,
-                type='line',
-                line=plotlyLayout.shape.Line(
-                    dash='dash',
-                    color='#2E5266',
-                    width=5
-                )
-            ),
-            dict(
-                xref='x',
-                yref='y',
-                y1=int(max(bin_val_max, y_val_max)) + 0.5,
-                y0=0,
-                x0=median_val,
-                x1=median_val,
-                type='line',
-                line=plotlyLayout.shape.Line(
-                    dash='dot',
-                    color='#BD9391',
-                    width=5
-                )
-            )
-        ]
+        )
     )
-    return Figure(data=[trace, trace1, trace2, trace3], layout=layout)
 
-# for histogram slider
-@app.callback(Output('bin-auto', 'values'), [Input('bin-slider', 'value')],
-              [State('dynamic-power', 'figure')],
-              [Event('bin-slider', 'change')])
-def deselect_auto(sliderValue, wind_speed_figure):
-    if (wind_speed_figure is not None and
-       len(wind_speed_figure['data'][0]['y']) > 5):
-        return ['']
-    else:
-        return ['Auto']
-
-# for histogram slider
-@app.callback(Output('bin-size', 'children'), [Input('bin-auto', 'values')],
-              [State('bin-slider', 'value')],
-              [])
-def deselect_auto(autoValue, sliderValue):
-    if 'Auto' in autoValue:
-        return '# of Bins: Auto'
-    else:
-        return '# of Bins: ' + str(int(sliderValue))
+    return go.Figure(data=[trace], layout=layout)
 
 
 # Callback for speed update
@@ -370,15 +247,15 @@ def get_temp(interval):
                             rowid > "{}" AND rowid <= "{}";'
                             .format(total_time-200, total_time), con)
 
-    trace = Scatter(
+    trace = go.Scatter(
         y=df['Speed'],
-        line=scatter.Line(
+        line= go.scatter.Line(
             color='#990000'
         ),
         mode='lines'
     )
 
-    layout = Layout(
+    layout = go.Layout(
         height=450,
         xaxis=dict(
             range=[0, 200],
@@ -398,14 +275,14 @@ def get_temp(interval):
             zeroline=False,
             nticks=max(6, round(df['Speed'].iloc[-1]/10))
         ),
-        margin=plotlyLayout.Margin(
+        margin=go.layout.Margin(
             t=45,
             l=50,
             r=50
         )
     )
 
-    return Figure(data=[trace], layout=layout)
+    return go.Figure(data=[trace], layout=layout)
 
 
 external_css = ["https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
